2d6efb78e2ba325eb0357151bcd7b6a624a53ac7
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-20 21:30:43 -0700

    added frontend login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -111,3 +111,34 @@ exports.activateAccount = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 }
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = generateToken({ id: user._id.toString() }, "7d");
+
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success! Please verify your email address...",
+
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
