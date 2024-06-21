48607c10e3a6f91970d4fd31f5ec6d3563fa2af2
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-20 22:04:17 -0700

    set up react router, loads but I dont see the names in the page, will fix later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import Home from './pages/home';
+
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-  get();
-  return <div>welcome to frontend</div>;
+  <Routes>
+    <Route path="/login" element={<Login />} exact />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/" element={<Home />} />
+  </Routes>
+
+
 }
 
 export default App;
