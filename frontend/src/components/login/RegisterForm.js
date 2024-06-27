a233d5637ad59b58a1f67f7ada3349f3d387b5a2
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-26 17:59:27 -0700

    setting up register

---
+++ frontend/src/components/login/RegisterForm.js
@@ -0,0 +1,14 @@
+
+export default function RegisterForm() {
+  return <div className="blur">
+    <div className="register">
+      <div className="register_header">
+        <i className="exit_icon"></i>
+        <span>Sign Up</span>
+        <span> its' quick an easy</span>
+      </div>
+    </div>
+  </div>
+
+}
+
